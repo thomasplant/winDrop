ba5ad51369623dac3a8f0b6a432ded8921bb17fe
Kevin Wai <kevinwai97@gmail.com>
2025-03-21 15:05:56 -0700

    Updated quic files

--- UDP/server_UDP.py
+++ UDP/server_UDP.py
@@ -44,7 +44,7 @@ def server_logic(total_clients, recieve_file_size):
                     with open(filename, 'bw') as fileopen:
                         #First recieve
                         if((b'<EOF/>' in data)):
-                            data = data.replace(b'<EOF/>', b'');
+                            data = data.replace(b'<EOF/>', b'')
                             fileopen.write(data)
                             log_message(f"Received file successfully! New filename: {filename}", start_time)
                         else:
@@ -90,7 +90,7 @@ def _log_message(message, start_time, end_time):
     log_text.insert(tk.END, "------------------------------------------------------------" + "\n")
     log_text.see(tk.END)
 
-host = "localhost" # This allows connections from any machine on the network
+host = "0.0.0.0" # This allows connections from any machine on the network
 port = 8080
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # (IPv4, UDP)
 sock.bind((host, port))
